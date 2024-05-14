1ee5b9055eb131dc5de09ac60b272e7b72a59cde
alroman30 <202110537@feualabang.edu.ph>
2024-05-14 16:18:04 +0800

    update

--- src/Main/Navigation/Routes/Dashboard.tsx
+++ src/Main/Navigation/Routes/Dashboard.tsx
@@ -5,12 +5,14 @@ import { MapContainer, TileLayer } from "react-leaflet";
 export default function Dashboard() {
   return (
     <div className="page-content dashboardMain">
+
       <MapContainer className="map" center={[14.1407, 121.4692]} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
+      
     </div>
   );
 }
