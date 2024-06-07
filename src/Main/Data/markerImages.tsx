83b1719e308161c1449f55b7bfd06253bb2db6a8
KyunnDayo <therealjustineromero@yahoo.com>
2024-06-07 13:55:25 +0800

    added Historical, removed balete

--- src/Main/Data/markerImages.tsx
+++ src/Main/Data/markerImages.tsx
@@ -1,13 +1,47 @@
 import Elvira from "../LocationImage/Elvira.jpg";
+import Candelaria from "../LocationImage/Candelaria.jpg";
+import Sebastian from "../LocationImage/sebastian.jpg";
+import Jobap from "../LocationImage/Jobap.jpg";
+import Mulawin from "../LocationImage/Mulawin.jpg";
+import JacintoBurial from "../LocationImage/JacintoBurial.jpg";
+import Imacon from "../LocationImage/ImmaculateConception.jpg";
+import Maimpis from "../LocationImage/Maimpis.jpg";
+import Gregory from "../LocationImage/SanGregorio.jpg";
+import Capricho from "../LocationImage/Puente del Capricho.jpg";
+import Joser from "../LocationImage/Jose Rizal Monument.jpg";
+import Santiago from "../LocationImage/The Roman Catholic Parish of San Santiago Apostol.jpg";
+import Tricross from "../LocationImage/Three Crosses of Paete.jpg";
+import Guadalupe from "../LocationImage/Diocesan Shrine and Parish of Our Lady of Guadalupe.jpg";
+import Alcantara from "../LocationImage/Saint Peter of Alcantara Parish Church.jpg";
+import Natividad from "../LocationImage/Nuestra Senora dela Natividad Parish.jpg";
+import Santonio from "../LocationImage/National Shrine and Parish of San Antonio.jpg";
 
 const markerImages = {
-  //Food
+  //SAMPLE--------------------------------------------------------------------------
   "Elvira's Creamy Halo-Halo and Pancit Lucban": Elvira,
-  //Gistorical
-  //Entertainemnt
-  //Nature
+  //Historical----------------------------------------------------------------------
+  "Nuestra Señora De La Candelaria Parish": Candelaria,
+  "San Sebastian Parish Church": Sebastian,
+  "Saint John the Baptist Parish": Jobap,
+  /*balete tree removed   
+    "name": "Balete Tree of Kalayaan",
+    "coordinates": [14.344003256661514, 121.48872530207747],
+    "type": "Historical",
+    "budget": "Low"
+  },*/
+  "Cathedral of Our Lady of Maulawin": Mulawin,
+  "Emilio Jacinto Burial Place and Monument": JacintoBurial,
+  "Immaculate Conception Parish Church": Imacon,
+  "Labanan sa Maimpis": Maimpis,
+  "St. Gregory the Great Parish Church": Gregory,
+  "Puente del Capricho": Capricho,
+  "Jose Rizal Monument": Joser,
+  "The Roman Catholic Parish of San Santiago Apostol": Santiago,
+  "Three Crosses of Paete": Tricross,
+  "Diocesan Shrine and Parish of Our Lady of Guadalupe": Guadalupe,
+  "Saint Peter of Alcantara Parish Church": Alcantara,
+  "Nuestra Senora dela Natividad Parish": Natividad,
+  "National Shrine and Parish of San Antonio": Santonio,
 };
 
 export default markerImages;
-
-//Good luck fred !
