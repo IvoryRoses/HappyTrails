eeaa902986e432c49ad4a3dd9285b38d4a858d4c
KyunnDayo <therealjustineromero@yahoo.com>
2024-06-08 12:20:17 +0800

    added Nature

--- src/Main/Data/markerImages.tsx
+++ src/Main/Data/markerImages.tsx
@@ -57,8 +57,8 @@ import Brickoven from "../LocationImage/The Brickoven Café.jpg";
 import Handb from "../LocationImage/Herbs And Beans.jpg";
 import Norma from "../LocationImage/Cafe Norma.jpg";
 import Konstrak from "../LocationImage/Konstrak coffee.jpg";
-import Speranza from "../LocationImage/Speranza Plus (ThebeanLab and Kaye's tea).jpg";
-import Leona from "../LocationImage/Leona's Kitchen.jpg";
+import Speranza from "../LocationImage/SperanzaPlus.jpg";
+import Leona from "../LocationImage/Leonas Kitchen.jpg";
 import Lunahills from "../LocationImage/Luna Hills Cafe Resort.jpg";
 import Babi from "../LocationImage/Babi Bear Samgyupsal.jpg";
 import Edong from "../LocationImage/Halo-halo ni Edong.jpg";
@@ -69,6 +69,40 @@ import Caleb from "../LocationImage/Caleb_s Kitchen.jpg";
 import Bossramen from "../LocationImage/Boss D Ramen.jpg";
 import Dashe from "../LocationImage/Dash Espresso.jpg";
 import Secretb from "../LocationImage/Secret Brew.jpg";
+import Pagsanjan from "../LocationImage/Pagsanjan Falls.jpg";
+import Cavinticave from "../LocationImage/Cavinti Underground River and Caves Complex.jpg";
+import Nakulo from "../LocationImage/Naculo Falls.jpg";
+import Bumbunganeco from "../LocationImage/Bumbungan Eco Park .jpg";
+import ESCavinti from "../LocationImage/Pueblo El Salvador Cavinti Nature_s Park and Picnic Grove.jpg";
+import Bumbungan2falls from "../LocationImage/Bumbungan Twin Falls.jpg";
+import Domelis from "../LocationImage/Domelis Campsite.jpg";
+import Hogad from "../LocationImage/Camp Hogad.jpg";
+import Kalayaan2falls from "../LocationImage/Kalayaan Twin Falls.jpg";
+import Tanawanvd from "../LocationImage/Tanawan View Deck.jpg";
+import Hulugan from "../LocationImage/Hulugan Falls.jpg";
+import Hiddenfalls from "../LocationImage/Hidden Falls.jpg";
+import MWwindmill from "../LocationImage/Pililla 54 MW Windmill.jpg";
+//import Mabitactree from "../LocationImage/mabitac"
+import Baysidebwalk from "../LocationImage/Bayside Baywalk.jpg";
+import Taytayfalls from "../LocationImage/Taytay Falls.jpg";
+import Redoorz from "../LocationImage/RedDoorz @ Hilarion_s Farm.jpg";
+import Aidaaurora from "../LocationImage/AIDA AURORA NATURES FARM.jpg";
+import Apazoo from "../LocationImage/APA Farms Mini Zoo.jpg";
+import Matabungca from "../LocationImage/Matabungca Falls.jpg";
+import Wawap from "../LocationImage/Wawa Park.jpg";
+import Talons from "../LocationImage/Talon Sirena.jpg";
+import Pingaspark from "../LocationImage/Mt. Pingas Pilgrimage Park.jpg";
+import Buntotp from "../LocationImage/Buntot Palos Falls.jpg";
+import Ambon2x from "../LocationImage/Ambon-Ambon Falls.jpg";
+import Bulusukanr from "../LocationImage/Bulusukan River & Eco Park.jpg";
+//import Aplayam from "../LocationImage/a"
+import Marilagfalls from "../LocationImage/MarilagFalls.jpg";
+import StamariaLVD from "../LocationImage/Sta. Maria Laguna Viewing Deck.jpg";
+import Lihimlagusan from "../LocationImage/Lihim na Lagusan.jpg";
+import Basekamp from "../LocationImage/Hammock BaseKamp.jpg";
+//import VFmanalo from "../LocationImage/Place"
+import Sampalocf from "../LocationImage/Sampaloc Falls.jpg";
+import TMfarm from "../LocationImage/Top Mount Farm.jpg";
 
 const markerImages = {
   //SAMPLE--------------------------------------------------------------------------
@@ -147,6 +181,42 @@ const markerImages = {
   "Dash Espresso": Dashe,
   "SECRET BREW": Secretb,
   //Nature------------------------------------------------------------------------
+  "Pagsanjan Falls": Pagsanjan,
+  "Cavinti Underground River and Caves Complex": Cavinticave,
+  "Nakulo Falls": Nakulo,
+  "Bumbungan Eco Park": Bumbunganeco,
+  "Pueblo El Salvador Cavinti Nature's Park and Picnic Grove": ESCavinti,
+  "Bumbungan Twin Falls": Bumbungan2falls,
+  "Domelis Campsite": Domelis,
+  "Camp Hogad": Hogad,
+  "Kalayaan Twin Falls": Kalayaan2falls,
+  "Tanawan View Deck": Tanawanvd,
+  "Hulugan Falls": Hulugan,
+  "Hulugan Falls Jump-off Point": Hulugan,
+  "Hidden Falls": Hiddenfalls,
+  "Pililla 54 MW Windmill": MWwindmill,
+  //"Balete Tree of Mabitac (Century Tree)" : Mabitactree,
+  "Bayside Baywalk": Baysidebwalk,
+  "Taytay Falls": Taytayfalls,
+  "RedDoorz @ Hilarion's Farm": Redoorz,
+  "AIDA AURORA NATURES FARM": Aidaaurora,
+  "APA Farms Mini Zoo": Apazoo,
+  "Matabungca Falls": Matabungca,
+  "Wawa Park": Wawap,
+  "Talon Sirena": Talons,
+  "Mt. Pingas Pilgrimage Park": Pingaspark,
+  "Buntot Palos Falls": Buntotp,
+  "Ambon-Ambon Falls": Ambon2x,
+  "Bulusukan River & Eco Park": Bulusukanr,
+  //"Aplaya Marker": Aplayam,
+  "Three Falls (Marilag Falls)": Marilagfalls,
+  "Sta. Maria Laguna Viewing Deck": StamariaLVD,
+  "Lihim na Lagusan": Lihimlagusan,
+  "Hammock BaseKamp": Basekamp,
+  // "VF Manalo's Place": VFmanalo,
+  "Sampaloc Falls": Sampalocf,
+  "Top Mount Farm": TMfarm,
+  //ENTERTAINMENT------------------------------------------------------------
 };
 
 export default markerImages;
